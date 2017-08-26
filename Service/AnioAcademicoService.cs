b4c2e660a5aa043605a6b8b936b2ea4868457520
luis <luis@LAPTOP-DHL76PGB>
2017-08-26 15:50:22 -0500

    AnioAcdemcoService

---
+++ Service/AnioAcademicoService.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Domain;
+using Repository;
+namespace Service
+{
+    class AnioAcademicoService : IAnioAcademicoService
+    {
+        private IAñoAcademicoRepository _anioRepository;
+        public AnioAcademicoService()
+        {
+            if (_anioRepository==null)
+            {
+                _anioRepository = new AnioAcademicoRepository();
+            }
+        }
+
+        public void AddAnioAcademico(AnioAcademico anioAcademico)
+        {
+            _anioRepository.AddAnioAcademico(anioAcademico);
+        }
+
+        public void DeleteAnioAcademico(int anioAcademico)
+        {
+            _anioRepository.DeleteAnioAcademico(anioAcademico);
+        }
+
+        public AnioAcademico GetAnioAcademico(int id)
+        {
+            return _anioRepository.GetAnioAcademico(id);
+        }
+
+        public IEnumerable<AnioAcademico> GetAnioAcademicos(string criterio)
+        {
+            return _anioRepository.GetAnioAcademicos(criterio);
+        }
+
+        public void UpdateAnioAcademico(AnioAcademico aniAcademico)
+        {
+            _anioRepository.UpdateAnioAcademico(aniAcademico);
+        }
+    }
+}
---
+++ Service/IAnioAcademicoService.cs
@@ -0,0 +1,22 @@
+﻿using Domain;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Service
+{
+   public interface IAnioAcademicoService
+    {
+        IEnumerable<AnioAcademico> GetAnioAcademicos(String criterio);
+
+        AnioAcademico GetAnioAcademico(Int32 id);
+
+        void AddAnioAcademico(AnioAcademico anioAcademico);
+
+        void UpdateAnioAcademico(AnioAcademico aniAcademico);
+
+        void DeleteAnioAcademico(Int32 anioAcademico);
+    }
+}
