e447625b3c8fcfdc378dd07374b3e7d4fd887bba
luis <luis@LAPTOP-DHL76PGB>
2017-08-28 17:20:17 -0500

    controlles matricula index

---
+++ Mvc5/Controllers/MatriculaController.cs
@@ -0,0 +1,158 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Service;
+using Domain;
+
+namespace Mvc5.Controllers
+{
+    public class MatriculaController : Controller
+    {
+        private IMatriculaService _matriculaService;
+        private IAlumnoService _alumnoService;
+        private IApoderadoService _apoderadoService;
+        private IAnioAcademicoService _anioAcademicoService;
+        private IUbigeoService _ubigeoService;
+        private IDocumentoService _documentoService;
+        private INivelService _nivelService;
+        private IGradoService _gradoService;
+        private ISeccionService _seccionService;
+        public MatriculaController()
+        {
+            if (_matriculaService ==null)
+            {
+                _matriculaService = new MatriculaService();
+            }
+            if (_alumnoService==null)
+            {
+                _alumnoService = new AlumnoService();
+
+            }
+            if (_apoderadoService ==null)
+            {
+                _apoderadoService = new ApoderadosService();
+            }
+            if (_anioAcademicoService==null)
+            {
+                _anioAcademicoService = new AnioAcademicoService();
+            }
+            if (_ubigeoService==null)
+            {
+                _ubigeoService = new UbigeoService();
+            }
+            if (_documentoService== null)
+            {
+                _documentoService = new DocumentoService();
+            }
+            if (_nivelService==null)
+            {
+                _nivelService = new NivelService();
+            }
+            if (_gradoService==null)
+            {
+                _gradoService = new GradoService();
+            }
+            if (_seccionService==null)
+            {
+                _seccionService = new SeccionService();
+            }
+        }
+
+        // GET: Matricula
+        public ActionResult Index()
+        {
+            ViewBag.alumno = new SelectList(new List<SelectListItem>
+            {
+                new SelectListItem() {Text= "seleccione", Value = "0" }
+            }, "Value", "Text");
+            var grados = new List<SelectListItem>() {
+                new SelectListItem() { Text = "Grado", Value = "0" }
+            };
+            var seccion = new List<SelectListItem>() {
+                new SelectListItem() { Text = "Seccion", Value = "0" }
+            };
+            ViewBag.anios = new SelectList(_anioAcademicoService.GetAnioAcademicos(""), "AnioAcademicoId", "Anio");
+
+            ViewBag.grado = new SelectList(grados, "Value", "Text");
+            ViewBag.seccion = new SelectList(seccion, "Value", "Text");
+            ViewBag.nivel = new SelectList(_nivelService.GetNiveles(), " NivelId ", "Nombre");
+            var fecha = DateTime.Now.Year.ToString();
+            var lista = _matriculaService.GetMatriculas("").Where(d => d.AnioAcademico.Anio.Equals(fecha));
+            return View(lista);
+        }
+
+        // GET: Matricula/Details/5
+        public ActionResult Details(int id)
+        {
+            return View();
+        }
+
+        // GET: Matricula/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Matricula/Create
+        [HttpPost]
+        public ActionResult Create(FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add insert logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: Matricula/Edit/5
+        public ActionResult Edit(int id)
+        {
+            return View();
+        }
+
+        // POST: Matricula/Edit/5
+        [HttpPost]
+        public ActionResult Edit(int id, FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add update logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: Matricula/Delete/5
+        public ActionResult Delete(int id)
+        {
+            return View();
+        }
+
+        // POST: Matricula/Delete/5
+        [HttpPost]
+        public ActionResult Delete(int id, FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add delete logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
---
+++ Mvc5/Scripts/zas/Matricula/Matricula.js
@@ -0,0 +1,113 @@
+﻿("#success-alert").fadeTo(2500, 500).slideUp(1000, function () {
+    $("#success-alert").slideUp(1000);
+});
+
+$("#btnAgregarMatricula").click(function () {
+    $.get("/Matricula/Create")
+    .done(function (data) {
+        if (data === "ok") {
+            toastr.info("El Periodo de Matriculas a terminado.");
+        }
+    });
+});
+
+//$('#IdAlumno').selectize({
+//    create: false,
+//    sortField: 'text',
+//});
+
+$("#nivel").change(function () {
+    $("#grado").empty();
+    $("#grado").html("<option value='0'>Grado</option>");
+    $("#seccion").empty();
+    $("#seccion").html("<option value='0'>Seccion</option>");
+    var idNivel = $(this).val();
+    $.post("/Matricula/GetNivel/?nivelId=" + idNivel)
+        .done(function (data) {
+            for (var i = 0; i < data.length; i++) {
+                $("#grado").append(
+                    "<option value='" + data[i].Id + "'>" + data[i].Nombre+ "</option>'"
+                );
+
+
+            }
+        });
+});
+
+$("#grado").change(function () {
+    var gradoId = $(this).val();
+    $("#seccion").empty();
+    $("#seccion").html("<option value='0'>Seccion</option>");
+    $.post("/Matricula/GetSeccion/?gradoId=" + gradoId)
+        .done(function (data) {
+            for (var i = 0; i < data.length; i++) {
+                $("#seccion").append(
+                    "<option value='" + data[i].Id + "'>" + data[i].Nombre+ "</option>'"
+                );
+
+
+            }
+        });
+
+    $.post("/Matricula/Index/?gradoId=" + gradoId)
+        .done(function (data) {
+            console.log(data);
+        });
+
+});
+
+$("#btnBuscaMatricula").click(function () {
+    var criterioAlumno = $("#criterioAlumno").val();
+    var grado = $("#grado").val();
+    var seccion = $("#seccion").val();
+    var nivel = $("#nivel").val();
+    var anio = $("#anioEscolar").val();
+
+    //var anioEscolar = $("#anioEscolar").val();
+    $.post("/Matricula/Index/?criterio=" +
+            criterioAlumno +
+            "&nivelId=" +
+            nivel +
+            "&gradoId=" +
+            grado +
+            "&seccionId=" +
+            seccion +
+            "&anioId=" +
+            anio)
+        .done(function (data) {
+            console.log(data);
+            if (data === "ok") {
+                toastr.success("No existen alumnos matriculdos en este Año escolar para esta Sección.");
+            }
+
+            $("#search_Matricula").html(data);
+        });
+});
+
+$("#criterioAlumno").keyup(function () {
+    var criterioAlumno = $("#criterioAlumno").val();
+    var grado = $("#grado").val();
+    var seccion = $("#seccion").val();
+    var nivel = $("#nivel").val();
+    var anio = $("#anioEscolar").val();
+
+    //var anioEscolar = $("#anioEscolar").val();
+    $.post("/Matricula/Index/?criterio=" +
+            criterioAlumno +
+            "&nivelId=" +
+            nivel +
+            "&gradoId=" +
+            grado +
+            "&seccionId=" +
+            seccion +
+            "&anioId=" +
+            anio)
+        .done(function (data) {
+            console.log(data);
+            if (data === "ok") {
+                toastr.success("No existen alumnos matriculdos en este Año escolar para esta Sección.");
+            }
+
+            $("#search_Matricula").html(data);
+        });
+});
